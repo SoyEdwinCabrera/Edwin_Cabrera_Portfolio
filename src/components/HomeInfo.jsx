7620b016a8c3cacdd95c923bf1fb0fa257497b0a
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-03-22 00:09:03 -0500

    Fox

--- src/components/HomeInfo.jsx
+++ src/components/HomeInfo.jsx
@@ -1,51 +1,63 @@
-import React from 'react'
+import React from 'react';
+
 import { Link } from 'react-router-dom';
-import { arrow } from '../assets/icons'
+import { arrow } from '../assets/icons';
+import PropTypes from 'prop-types';
 
 const InfoBox = ({ text, link, btnText }) => (
     <div className='info-box'>
         <p className='font-medium sm:text-xl text-center'>{text}</p>
         <Link to={link} className='neo-brutalism-white neo-btn' >
-            {btnText}        
+            {btnText}
             <img src={arrow} className= "w-4 h-4 object-contain"/>
         </Link>
     </div>
 )
 
+InfoBox.propTypes = {
+    text: PropTypes.string.isRequired,
+    link: PropTypes.string.isRequired,
+    btnText: PropTypes.string.isRequired
+};
+
 const renderContent = {
-    1: (
-        <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
-            Hi, I am <span className="font-semibold">Edwin</span>👋🏻
-            <br />
-            I am a Data Analyst from Colombia
-        </h1>
-    ),
-    2: (
-        <InfoBox 
-            text="Worked with many companies and picked up many skills along the way"
-            link="/projects"
-            btnText="Learn more"
-        />
-    ),
-    3: (
-        <InfoBox 
-            text="Led multiple projects to success over the years. Curious about the impact?"
-            link="/about"
-            btnText="Visit my portfolio"
-       />
-    ),
-    4: (
-        <InfoBox 
-            text="Need a project done or looking for a dev? I'm just a few keystrokes away"
-            link="/contact"
-            btnText="Let's talk"
-       />
-    ),
-}
+  1: (
+    <h1 className='sm:text-xl sm:leading-snug text-center neo-brutalism-blue py-4 px-8 text-white mx-5'>
+      Hi, I am <span className="font-semibold">Edwin</span>👋🏻
+      <br />
+      I am a Data Analyst from Colombia
+    </h1>
+  ),
+  2: (
+    <InfoBox
+      text="Worked with many companies and picked up many skills along the way"
+      link="/projects"
+      btnText="Learn more"
+    />
+  ),
+  3: (
+    <InfoBox
+      text="Led multiple projects to success over the years. Curious about the impact?"
+      link="/about"
+      btnText="Visit my portfolio"
+    />
+  ),
+  4: (
+    <InfoBox
+      text="Need a project done or looking for a dev? I'm just a few keystrokes away"
+      link="/contact"
+      btnText="Let's talk"
+    />
+  ),
+};
 
 
-const HomeInfo = ({currentStage}) => {
-    return renderContent[currentStage] || null;
-}
+const HomeInfo = ({ currentStage }) => {
+  return renderContent[currentStage] || null;
+};
 
-export default HomeInfo
+HomeInfo.propTypes = {
+    currentStage: PropTypes.number
+};
+
+export default HomeInfo;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,16 +1,26 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef, Suspense } from 'react';
 import emailjs, { sendForm } from '@emailjs/browser';
-const Contact = () => {
-  const formRef = useRef(null);
-  const [form, setForm] = useState({name: '', email: '', message: ''})
-  const [isLoading, setIsLoading] = useState(false);
+import {Canvas} from '@react-three/fiber';
+import { Html } from '@react-three/drei';
 
-  const handleChange = (e) => { 
-    setForm({...form, [e.target.name]: e.target.value})
+import Loader from '/src/components/Loader.jsx';
+import Fox from '../models/Fox';
+
+const Contact = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
+  const [currentAnimation, setCurrentAnimation] = useState('idle');
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
   };
+
+  
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsLoading(true);
+    setCurrentAnimation('hit');
 
     emailjs.send(
       import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
@@ -28,61 +38,67 @@ const Contact = () => {
       //TODO: Show success message
       //TODO: Hide an alert
 
-      setForm({ name: '', email: '', message: '' });
+      setTimeout(() => {
+        setCurrentAnimation('idle')
+        
+        setForm({ name: '', email: '', message: '' });
+      }, [3000])
+      
     }).catch((error) => {
       setIsLoading(false);
+      setCurrentAnimation('idle')
       console.log(error);
       //TODO: Show error message
     })
-   };
+  };
 
-  const handleFocus = () => { };
-  const handleBlur = () => { };
+  const handleFocus = () => setCurrentAnimation('walk');
+  const handleBlur = () => setCurrentAnimation('idle');
   
   return (
     <section className="relative flex lg:flex-row flex-col max-container">
       <div className="flex-1 min-w-[50%] flex flex-col">
         <h1 className="head-text">Get in Touch</h1>
 
-        <form 
-        className="w-full flex flex-col gap-7 mt-14"
-        onSubmit={handleSubmit}
+        <form
+          className="w-full flex flex-col gap-7 mt-14"
+          onSubmit={handleSubmit}
         >
           <label className="text-black-500 font-semibold">
             Name
-          <input 
-            type="text"
-            name="name"
-            className="input"
-            placeholder="Your name"
-            required
-            value={form.name}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-          </label>        
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="Your name"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
           <label className="text-black-500 font-semibold">
             Email
-          <input 
-            type="email"
-            name="email"
-            className="input"
-            placeholder="youremail@email.com"
-            required
-            value={form.email}
-            onChange={handleChange}
-            onFocus={handleFocus}
-            onBlur={handleBlur}
-          />
-          </label>        
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="youremail@email.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
           <label className="text-black-500 font-semibold">
             Your Message
-            <textarea 
+            <textarea
               name="message"
               rows={9}
               className="textarea"
-              placeholder="Let me know how I can help you!"
+              placeholder="Let me know if we can work together!"
               required
               value={form.message}
               onChange={handleChange}
@@ -101,8 +117,42 @@ const Contact = () => {
           </button>
         </form>
       </div>
+      <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas
+          camera={{
+            position: [0, 0, 5],
+            fov: 75,
+            near: 0.1,
+            far: 1000
+          }}
+        >
+          <directionalLight intensity={2.5} position={[0, 0, 1]} />
+          <ambientLight intensity={0.5} />
+          <Suspense fallback={<Loader />}>
+            <Fox
+              currentAnimation={currentAnimation}
+              position={[0.5, 0.35, 0]}
+              rotation={[12.6, -0.6, 0]}
+              scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div>
+      {/* <div className='lg:w-1/2 w-full lg:h-auto md:h-[550px] h-[350px]'>
+        <Canvas camera={{
+          position: [0, 0, 5]
+        }}>
+          <Suspense fallback={<Loader/>}>
+            <Fox
+            position={[0.5, 0.35, 0]}
+            rotation={[12, 0, 0]}
+            scale={[0.5, 0.5, 0.5]}
+            />
+          </Suspense>
+        </Canvas>
+      </div> */}
     </section>
   )
-}
+};
 
 export default Contact;

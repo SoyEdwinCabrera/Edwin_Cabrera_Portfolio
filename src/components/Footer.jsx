8d6c2c96773b503a26033263fa49bd426f833779
SoyEdwinCabrera <139647637+SoyEdwinCabrera@users.noreply.github.com>
2025-05-05 22:45:57 -0500

    Se agrega footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+   const Footer = () => {
+     return (
+       <footer style={footerStyle}>
+         <p> Edwin Cabrera &copy; 2025</p>
+       </footer>
+     );
+   };
+
+   const footerStyle = {
+     backgroundColor: '#2b77e7',
+     color: '#fff',
+     textAlign: 'center',
+     padding: '13px 0',
+     position: 'fixed',
+     bottom: 0,
+     width: '100%',
+     fontFamily: 'Poppins, sans-serif',
+     fontSize: '16px',
+   };
+
+   export default Footer;
